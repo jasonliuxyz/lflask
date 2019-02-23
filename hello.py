0b7b0ba326e26f55a901a091c637612c22c84cac
jasonliuxyz <15190451129@163.com>
2019-02-23 15:42:18 +0800

    11

--- hello.py
+++ hello.py
@@ -1,15 +1,41 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	users = db.relationship('User', backref='role')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
 
 class NameForm(Form):
 	name = StringField('What is your name?', validators=[DataRequired()])
@@ -19,9 +45,16 @@ class NameForm(Form):
 def index():
 	form = NameForm()
 	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user = User(username=form.name.data)
+			db.session.add(user)
+			session['know'] = False
+		else:
+			session['know'] = True
 		session['name'] = form.name.data
 		return redirect(url_for('index'))
-	return render_template('index.html', form=form, name=session.get('name'))
+	return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 @app.errorhandler(404)
 def page_not_found(e):
